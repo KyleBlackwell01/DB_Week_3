019aea71802ae11e36e208d96227472db755b7c1
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-08-13 09:13:50 +1000

    Task 2 Completed

--- db_week_3_tasks.sql
+++ db_week_3_tasks.sql
@@ -18,3 +18,46 @@ FOREIGN KEY (ClientID) REFERENCES Client
 
 */
 
+DROP TABLE IF EXISTS dbo.Booking;
+DROP TABLE IF EXISTS dbo.Event;
+DROP TABLE IF EXISTS dbo.Client;
+DROP TABLE IF EXISTS dbo.Tour;
+
+CREATE TABLE Tour 
+(
+    TourName VARCHAR(5) PRIMARY KEY,
+    Description VARCHAR(100)
+)
+
+CREATE TABLE Client
+(
+    ClientID INT PRIMARY KEY,
+    Surname VARCHAR(50),
+    GivenName VARCHAR(50),
+    Gender VARCHAR(2)
+)
+
+CREATE TABLE Event
+(
+    EventYear INT,
+    EventMonth VARCHAR(3),
+    EventDay INT,
+    EventFee INT,
+    TourName VARCHAR(5),
+    PRIMARY KEY (EventYear,EventMonth,EventDay,TourName),
+    FOREIGN KEY (TourName) REFERENCES Tour
+)
+
+CREATE TABLE Booking
+(
+    DateBooked Date,
+    Payment INT,
+    EventYear INT,
+    EventMonth VARCHAR(3),
+    EventDay INT,
+    TourName VARCHAR(5),
+    ClientID INT,
+    PRIMARY KEY(EventYear,EventMonth,EventDay,TourName,ClientID),
+    FOREIGN KEY (EventYear,EventMonth,EventDay,TourName) REFERENCES Event,
+    FOREIGN KEY (ClientID) REFERENCES Client
+)
